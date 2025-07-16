2d4853c8f68d0b4c5f971e0f3cab3dc8d1d52604
atanu <atanurkm11@gmail.com>
2025-07-16 16:48:38 -0600

    normalize_avi_filenames

--- Python_scripts/Utility_functions/normalize_avi_filenames.py
+++ Python_scripts/Utility_functions/normalize_avi_filenames.py
@@ -1,12 +1,12 @@
 import os
 
-input_dir = r"C:\DeepLabCutProjects\data\ToyNEW RatVideos\10-16-23"
-
-for filename in os.listdir(input_dir):
-    if filename.lower().endswith('.avi'):
-        new_name = filename.replace(' - Copy', '').replace('  ', ' ').replace(' ', '_')
-        src = os.path.join(input_dir, filename)
-        dst = os.path.join(input_dir, new_name)
-        if src != dst:
-            os.rename(src, dst)
-            print(f"Renamed: {filename} -> {new_name}")
+def normalize_avi_filenames(input_dir):
+    """Rename .avi files in the given folder by replacing spaces and cleanup."""
+    for filename in os.listdir(input_dir):
+        if filename.lower().endswith('.avi'):
+            new_name = filename.replace(' - Copy', '').replace('  ', ' ').replace(' ', '_')
+            src = os.path.join(input_dir, filename)
+            dst = os.path.join(input_dir, new_name)
+            if src != dst:
+                os.rename(src, dst)
+                print(f"Renamed: {filename} -> {new_name}")
