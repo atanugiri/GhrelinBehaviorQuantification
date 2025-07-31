12ded90ba935100ae6d054e68206e718476d75be
atanu <atanurkm11@gmail.com>
2025-07-30 22:31:57 -0600

    Given a video filename, search for the corresponding DLC CSV in DlcData subfolders

--- Python_scripts/Extract_db_columns/find_csv_for_video.py
+++ Python_scripts/Extract_db_columns/find_csv_for_video.py
@@ -1,19 +1,17 @@
 from pathlib import Path
 
-base_data_dir = Path(r"C:\DeepLabCutProjects\data")  # Adjust if needed
+# base_data_dir = Path(r"C:\DeepLabCutProjects\data")  # Adjust if needed
+base_data_dir = Path.home() / "Downloads" / "data" / "FoodOnly" / "DlcDataPytorchFiltered"
 
 def find_csv_for_video(video_name):
     """
     Given a video filename, search for the corresponding DLC CSV in DlcData subfolders.
     """
     stem = Path(video_name).stem  # remove .mp4 or .avi
-    pattern = f"{stem}DLC_resnet50_*.csv"
+    pattern = f"{stem}*_filtered.csv"
 
-    for subdir in base_data_dir.iterdir():
-        dlc_dir = subdir / "DlcData"
-        if dlc_dir.exists():
-            matches = list(dlc_dir.glob(pattern))
-            if matches:
-                return str(matches[0])  # return full path as string
+    matches = list(base_data_dir.glob(pattern))
+    if matches:
+        return str(matches[0])  # Return full path as string
 
-    return None  # if no match found
+    return None  # If no match found
